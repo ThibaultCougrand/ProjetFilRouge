84d596db9ad8bcf55d613a5f0839eb36a24e8a93
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-03-13 16:21:25 +0100

    ajout des link navbar, des couleurs chartes graphique, commit num2

--- vue/navbar.php
+++ vue/navbar.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-    <li>Presentation</li>
-    <li>Nos recettes</li>
-    <li>Connexion</li>
-    <li>Panier</li>
+    <li><a href="#">Presentation</li></a>
+    <li><a href="#">Nos recettes</li></a>
+    <li><a href="#">Connexion</li></a>
+    <li><a href="#">Panier</li></a>
 </ul>
 </nav>
