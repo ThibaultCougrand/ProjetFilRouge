10c0093d621f65cec3a9d0679ca605f62c2cd389
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-03-13 16:09:04 +0100

    Supression des commentaires et codes générer par netbeans, rectification du bug des liens d'image du header et déplacement dans l'index de la variable image

--- controler/controler-content.php
+++ controler/controler-content.php
@@ -1,35 +1,4 @@
 <?php
-
-//switch ($loc) {
-//    case 'recipes':
-//        $fileName, $image = 'recipes';
-//        break;
-//    case 'one-recipe':
-//        include "vue/one-recipe.php";
-//        $image = 'recipes';
-//        break;
-//    case 'ingredients':
-//        include "vue/ingredients.php";
-//        break;
-//    case 'sign-in':
-//        include "vue/sign-in.php";
-//        break;
-//    case 'sign-up':
-//        include "vue/sign-up.php";
-//        break;
-//    case 'account':
-//        include "vue/account.php";
-//        break;
-//    case 'cart':
-//        include 'vue/cart.php';
-//        break;
-//    case 'resume-cart':
-//        include 'vue/resume-cart';
-//        break
-//    default:
-//        include 'vue/home.php';
-//        break;
-//}
 $file = "vue/".$loc.".php";
 if (file_exists($file)) {
     include $file;
@@ -37,10 +6,7 @@ if (file_exists($file)) {
     echo 'erreur dans controler-content';
     die;
 }
-$image = "asset/".$loc.".jpg";
-if (!file_exists($image)) {
-    $image = "asset/default.jpg";
-}
+
 
 
 
--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php 
-//if (isset($_GET['loc']) && !empty($_GET['loc'])) {
-//    $loc = $_GET['loc'];
-//} else {
-//    $loc = 'home';
-//}
 $loc = filter_input(INPUT_GET, 'loc');
 if(!$loc){
     $loc = 'home';
 }
-?>
-<?php include "vue/template.php";
+
+$image = "asset/".$loc.".jpg";
+if (!file_exists($image)) {
+    $image = "asset/home.jpg";
+}
+include "vue/template.php";
