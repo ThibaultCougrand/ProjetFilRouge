7322443f7830945a5e5079f257b08bf976bdbca4
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-03-13 15:10:15 +0100

    ajout de la creation navbar etape1

--- vue/navbar.php
+++ vue/navbar.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<nav>
+    <ul>
+    <li>Presentation</li>
+    <li>Nos recettes</li>
+    <li>Connexion</li>
+    <li>Panier</li>
+</ul>
+</nav>
