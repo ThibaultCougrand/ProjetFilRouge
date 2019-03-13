394fcbb19b4ff13e772f3301d81634fc0fb85365
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-03-13 16:31:25 +0100

    ajmodification couleur et attribution des liens navbar, commit num3

--- vue/navbar.php
+++ vue/navbar.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
-    <li><a href="#">Presentation</li></a>
-    <li><a href="#">Nos recettes</li></a>
-    <li><a href="#">Connexion</li></a>
-    <li><a href="#">Panier</li></a>
+    <li><a href="index.php?loc=home.php">Accueil</li></a>
+    <li><a href="index.php?loc=recipes.php">Nos recettes</li></a>
+    <li><a href="index.php?loc=sign-in.php">Connexion</li></a>
+    <li><a href="index.php?loc=cart.php">Panier</li></a>
 </ul>
 </nav>
