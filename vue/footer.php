179dbae09b86867f84f0e31553a1c1450ee95835
Papytuc1 <kevinsanmartino@gmail.com>
2019-03-13 16:04:25 +0100

    couleur sur le footer et taille d'incone  modifier

--- vue/footer.php
+++ vue/footer.php
@@ -1,5 +1,5 @@
 <footer>
-<i class="fab fa-facebook fa-2x"></i>
-<i class="fab fa-twitter-square fa-2x"></i>
-<i class="fab fa-instagram fa-2x"></i>
+<i class="fab fa-facebook fa-4x"></i>
+<i class="fab fa-twitter-square fa-4x"></i>
+<i class="fab fa-instagram fa-4x"></i>
 </footer>
