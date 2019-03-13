13b2fbf58ffdc87aa1bab0b7a2deefbf0048c81d
Papytuc1 <kevinsanmartino@gmail.com>
2019-03-13 16:41:05 +0100

    flex sur le footer et mise en plase des couleleurs

--- vue/footer.php
+++ vue/footer.php
@@ -1,5 +1,10 @@
 <footer>
+    <div class="icones">
 <i class="fab fa-facebook fa-4x"></i>
 <i class="fab fa-twitter-square fa-4x"></i>
 <i class="fab fa-instagram fa-4x"></i>
+</div>
+<div class="mention">
+<h2><a href="vue/mentions-legale.html">mentions légale</a></h2>
+</div>
 </footer>
