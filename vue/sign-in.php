d2f505644bc449c8ccb4864cc7de942f1f719cc5
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-03-14 13:12:32 +0100

    Création du HTML de la page connection/inscription

--- vue/sign-in.php
+++ vue/sign-in.php
@@ -1,1 +1,27 @@
-
+<div class="contener-flex">
+    <div class="connect">
+        <form class="form-connect">
+            email:
+            <input type="email">
+            mot de passe:
+            <input type="password">
+            <input type="button" value="Se connecter">
+        </form>
+    </div>
+    <div class="divider"></div>
+    <div class="inscrition">
+        <form class="form-inscription">
+            nom:
+            <input type="text">
+            prenom:
+            <input type="text">
+            email:
+            <input type="email">
+            mot de passe:
+            <input type="password">
+            verifier votre mot de passe:
+            <input type="password">
+            <input type="button" value="S'inscrire"
+        </form>
+    </div>
+</div>
