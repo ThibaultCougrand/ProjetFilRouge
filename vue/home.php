2c1ead777f6226b3d3f7ecc5186981c1d5390273
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-03-14 11:31:54 +0100

    ajout de la structure page HOME et son CSS

--- vue/home.php
+++ vue/home.php
@@ -1,1 +1,11 @@
-<h1>hello world</h1>
+<article class="presentation">
+<aside>
+<img src="asset/presentation.jpg" alt="presentation entreprise">
+</aside>
+<div class="text-home">
+<h1>Presentation de notre entreprise</h1>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum mollitia reiciendis labore eligendi? Sit delectus consectetur ratione illo nostrum animi expedita voluptates beatae aliquid, sequi laborum, blanditiis soluta maxime quasi?</p>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora error distinctio fugiat excepturi maxime provident ea vitae quod explicabo voluptatibus corporis maiores qui, perspiciatis quaerat mollitia reprehenderit saepe obcaecati ut.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum libero, illo quo, est distinctio earum quidem magnam nulla quisquam optio animi officia aliquam quam velit, enim facilis blanditiis possimus alias?</p>
+</div>
+</article>
