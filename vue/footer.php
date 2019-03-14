2c5d959d0b4c1e0ee311e67eaedebcdbff1eb5be
Papytuc1 <kevinsanmartino@gmail.com>
2019-03-14 09:06:19 +0100

    correction d'une petite erreur dans le style.css

--- vue/footer.php
+++ vue/footer.php
@@ -1,10 +1,10 @@
 <footer>
     <div class="icones">
-<a href="#"><i class="fab fa-facebook fa-4x"></i></a>
-<a href="#"><i class="fab fa-twitter-square fa-4x"></i></a>
-<a href="#"><i class="fab fa-instagram fa-4x"></i></a>
-</div>
-<div class="mention">
-<h2><a href="vue/mentions-legale.html">mentions légale</a></h2>
-</div>
-</footer>
+        <a href="#"><i class="fab fa-facebook fa-4x"></i></a>
+        <a href="#"><i class="fab fa-twitter-square fa-4x"></i></a>
+        <a href="#"><i class="fab fa-instagram fa-4x"></i></a>
+    </div>
+    <div class="mention">
+        <h2><a href="vue/mentions-legale.html">mentions légale</a></h2>
+    </div>
+</footer> 
--- vue/navbar.php
+++ vue/navbar.php
@@ -1,9 +1,9 @@
 <nav>
     <ul>
-    <li><a href="index.php?loc=home">Accueil</li></a>
-    <li><a href="index.php?loc=recipes">Nos recettes</li></a>
-    <li><a href="index.php?loc=ingredients">Produits</li></a>
-    <li><a href="index.php?loc=sign-in">Connexion</li></a>
-    <li><a href="index.php?loc=cart">Panier</li></a>
-</ul>
-</nav>
+        <li><a href="index.php?loc=home">Accueil</li></a>
+        <li><a href="index.php?loc=recipes">Nos recettes</li></a>
+        <li><a href="index.php?loc=ingredients">Produits</li></a>
+        <li><a href="index.php?loc=sign-in">Connexion</li></a>
+        <li><a href="index.php?loc=cart">Panier</li></a>
+    </ul>
+</nav> 
