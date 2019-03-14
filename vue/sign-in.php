9091550e53d51e5a3f13187e1adddffea671c172
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-03-14 22:26:47 +0100

    Création du modele et class de page de connection

--- vue/sign-in.php
+++ vue/sign-in.php
@@ -1,16 +1,16 @@
 <div class="contener-flex-sign-in">
     <div><h3>Connexion</h3></div>
     <form class="connection">
-        <label>email:</label>
-        <input type="email">
-        <label>mot de passe:</label>
-        <input type="password">
-        <br><input type="button" value="Se connecter">
+        <label for="email">email:</label>
+        <input type="email" id="email" name="emailCo">
+        <label for="password">mot de passe:</label>
+        <input type="password" id="password" name="passwordCo">
+        <br><button type="submit">Se connecter</button>
     </form>
     <div class="divider"></div>
     <div><h3>Inscription</h3></div>
     <form class="inscription">
-        <label>nom:</label>
+        <label for="name">nom:</label>
         <input type="text">
         <label>prenom:</label>
         <input type="text">
@@ -20,6 +20,6 @@
         <input type="password">
         <label>verifier votre mot de passe:</label>
         <input type="password">
-        <br><input type="button" value="S'inscrire"
+        <br><button type="submit">S'inscrire</button>
     </form>
 </div>
