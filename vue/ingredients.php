602b2f9d62be1f81e3c73538fd89b6a5295722d6
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-03-14 14:02:08 +0100

    ajout de la page ingrédients, creation de article aside et div, positionnement en flex general et grid pour les figures et le css qui va avec updt num1

--- vue/ingredients.php
+++ vue/ingredients.php
@@ -1,1 +1,43 @@
-
+<article class="container-ing">
+    <aside>
+        <p>ici categorie ingrédient</p>
+    </aside>
+    <div class="ingredient">
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+        <figure>
+            <img src="asset/recipes.jpg" alt="">
+            <figcaption>Ici ingrédient</figcaption>
+        </figure>
+    </div>
+</article> 
