d01e279a8f2ecb78e5e9455ae6d2fc0e6647ef60
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-03-14 13:55:33 +0100

    CSS de la page connexion/inscription

--- vue/sign-in.php
+++ vue/sign-in.php
@@ -1,27 +1,25 @@
-<div class="contener-flex">
-    <div class="connect">
-        <form class="form-connect">
-            email:
-            <input type="email">
-            mot de passe:
-            <input type="password">
-            <input type="button" value="Se connecter">
-        </form>
-    </div>
+<div class="contener-flex-sign-in">
+    <div><h3>Connexion</h3></div>
+    <form class="connection">
+        <label>email:</label>
+        <input type="email">
+        <label>mot de passe:</label>
+        <input type="password">
+        <br><input type="button" value="Se connecter">
+    </form>
     <div class="divider"></div>
-    <div class="inscrition">
-        <form class="form-inscription">
-            nom:
-            <input type="text">
-            prenom:
-            <input type="text">
-            email:
-            <input type="email">
-            mot de passe:
-            <input type="password">
-            verifier votre mot de passe:
-            <input type="password">
-            <input type="button" value="S'inscrire"
-        </form>
-    </div>
+    <div><h3>Inscription</h3></div>
+    <form class="inscription">
+        <label>nom:</label>
+        <input type="text">
+        <label>prenom:</label>
+        <input type="text">
+        <label>email:</label>
+        <input type="email">
+        <label>mot de passe:</label>
+        <input type="password">
+        <label>verifier votre mot de passe:</label>
+        <input type="password">
+        <br><input type="button" value="S'inscrire"
+    </form>
 </div>
