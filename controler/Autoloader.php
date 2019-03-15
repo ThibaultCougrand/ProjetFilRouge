4477f601ccaceb2951530302020636f4d9ada38f
Papytuc1 <kevinsanmartino@gmail.com>
2019-03-15 11:50:15 +0100

    autoloader fonctionel

--- controler/Autoloader.php
+++ controler/Autoloader.php
@@ -1,11 +1,14 @@
 <?php
-class Autoloader{
-static function autoload($className){
-    include "modele/".$className.".php";
+class Autoloader
+{
+    static function autoload($className)
+    {
+        include "modele/" . $className . ".php";
+    }
+    static function register()
+    {
 
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
 }
-static function register(){
 
-    spl_autoload_register(array(__CLASS__,'autoload'));
-}
-}
--- controler/controler-recipes.php
+++ controler/controler-recipes.php
@@ -1,8 +1,5 @@
 <?php 
-include_once 'modele/ClassConnexion.php';
-include_once 'modele/ClassRecette.php';
 include_once 'controler/Autoloader.php';
 Autoloader::register();
-/* include_once 'modele/ModeleRecette.php'; */
 $modeleRecette = new ModeleRecette();
 $recettes = $modeleRecette->recipeCategory();
