555ea01dd002a05c48996a30a89f9e3a0ea4b20a
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-03-15 11:28:14 +0100

    Supression des fichier inutiles

--- vue/navbar.php
+++ vue/navbar.php
@@ -3,7 +3,7 @@
         <li><a href="index.php?loc=home">Accueil</li></a>
         <li><a href="index.php?loc=recipes">Nos recettes</li></a>
         <li><a href="index.php?loc=ingredients">Produits</li></a>
-        <li><a href="index.php?loc=sign-in">Connexion</li></a>
+        <li><a href="index.php?loc=sign-in-up">Connexion</li></a>
         <li><a href="index.php?loc=cart">Panier</li></a>
     </ul>
 </nav> 
