8430811187c29533bae42451bf4b52a1bd556568
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-03-18 19:27:17 +0100

    ajout de rien de fou une journée dans le vide sur produits

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ $(".un-filtre").click(function () {
                     console.log(element);
                     figure = document.createElement('figure');
                     lien = document.createElement('a');
-                    lien.href = "index.php?loc=one-recipe&id="+element.id;
+                    lien.href = "index.php?loc=one-recipe&id=" + element.id;
                     img = document.createElement('div');
                     figcaption = document.createElement('figcaption');
-                    img.className ="image-recette";
-                    img.style = "background-image: url("+element.img+")";
+                    img.className = "image-recette";
+                    img.style = "background-image: url(" + element.img + ")";
                     figcaption.textContent = element.name;
                     lien.append(img);
                     figure.append(lien);
@@ -30,6 +30,42 @@ $(".un-filtre").click(function () {
         }
     )
 })
-$(".image-recette").click(function(){
+$(".image-recette").click(function () {
 
+})
+
+/********************ICI REQUÊTE ROMAIN *******************************/
+$('un-filtre').click(function () {
+    reset = $('.container-ing');
+    reset.html(""); //me permet de reset la class qui contient les images, names etc
+    $.post(
+        'ajax.php', { //permet de pointe vers la page ajax.php
+            id: $(this).data("id"),
+            uc: 'produit'
+        },
+        function (data) {
+            results = (JSON.parse(data));
+            if (Array.isArray(results)) {
+                results.forEach(element => {
+                    figure = document.createElement('figure');
+                    lien = document.createElement('a');
+                    lien.href = "index.php?loc=produits&id=" + element.id;
+                    img = document.createElement('div');
+                    figcaption = document.createElement('figcaption');
+                    img.className = "image-recette";
+                    img.style = "background-image: url(" + element.img + ")";
+                    figcaption.textContent = element.name;
+                    lien.append(img);
+                    figure.append(lien);
+                    figure.append(figcaption);
+                    $('container-ing').append(figure);
+
+                });
+            } else {
+                console.log('requête ajax romain null');
+            }
+        })
+        $(".image-recette").click(function () {
+
+        })
 })
