f46295427e442798e7fd1078a32f3bf2863fb84d
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-03-19 09:17:52 +0100

    ajout du add

---
+++ modele/ModeleArticle.php
@@ -0,0 +1,70 @@
+<?php
+class ModeleArticle extends ClassConnexion
+{
+    public function articlesByCategory($id)
+    {
+        $array = [];
+        $req = parent::$bdd->prepare("SELECT ingredients.name, article.qtx, article.prix,
+         unit.unite, article.id, article.image 
+        FROM `category` 
+        JOIN ingredients ON category.id = ingredients.id_category_ing 
+        JOIN article ON ingredients.id = article.id_ing
+        JOIN unit ON article.id_unite = unit.id
+        WHERE category.id = :id");
+        $req->bindParam(':id', $id);
+        $req->execute();
+        while ($donnee = $req->fetch()) {
+            $articles = new ClassArticle();
+            $articles->setId($donnee['id']);
+            $articles->setName($donnee['name']);
+            $articles->setQtx($donnee['qtx']);
+            $articles->setPrix($donnee['prix']);
+            $articles->setUnit($donnee['unite']);
+            $articles->setImage($donnee['image']);
+            
+            array_push($array, $articles);
+        }
+        return $array;
+    }
+
+    public function categoryOfIngredients()
+    {
+        $array = [];
+        $req = parent::$bdd->query("SELECT category.name, category.id 
+        FROM `category_ing` JOIN category ON category_ing.id = category.id");
+        while ($donnee = $req->fetch()) {
+            $ingredient = new ClassIngredients();
+            $ingredient->setId($donnee['id']);
+            $ingredient->setName($donnee['name']);
+            array_push($array, $ingredient);
+        }
+        return $array;
+    }
+
+
+    public function articlesAll()
+    {
+        $array = [];
+        $req = parent::$bdd->prepare("SELECT ingredients.name, article.qtx, article.prix, unit.unite, article.id, article.image 
+        FROM `category` 
+        JOIN ingredients ON category.id = ingredients.id_category_ing 
+        JOIN article ON ingredients.id = article.id_ing
+        JOIN unit ON article.id_unite = unit.id
+        ORDER BY id DESC LIMIT 9");
+        $req->execute();
+        while ($donnee = $req->fetch()) {
+            $articles = new ClassArticle();
+            $articles->setId($donnee['id']);
+            $articles->setName($donnee['name']);
+            $articles->setQtx($donnee['qtx']);
+            $articles->setPrix($donnee['prix']);
+            $articles->setUnit($donnee['unite']);
+            $articles->setImage($donnee['image']);
+            
+            array_push($array, $articles);
+        }
+        return $array;
+    }
+    
+}
+
