0698b89fbb86d68f55a2adb13e7d1cf09e30b78b
Papytuc1 <kevinsanmartino@gmail.com>
2019-03-20 09:53:13 +0100

    css en cours sur la page panier

--- controler/controler-cart.php
+++ controler/controler-cart.php
@@ -1,7 +1,5 @@
 <?php
 $cookie = $_COOKIE['panier'];
 $data = json_decode($cookie,true);
-foreach ($data['recette'] as $key => $value) {
-    /* echo ' recette numero : '.$key.' x';
-    print_r($value); */
-}
+$recette = new ModeleOneRecipe();
+$cart = new ModeleCart();
--- vue/cart.php
+++ vue/cart.php
@@ -1,2 +1,17 @@
-<?php
-
+<article class="container-panier">
+    <h1 class="titre-panier">Votre panier</h1>
+    <?php
+    foreach ($data['recette'] as $key => $value) {
+        $list = $recette->ingredientsList($key);
+        $uneRecette = $recette->recipe($key);
+        echo '<h2 class="titre-recette">' . $uneRecette->name() . '</h2>';
+        echo '<ul class="ul-panier">';
+        foreach ($list as $ingredient) {
+            echo '<li class="taille-filtre un-ingredient ingredient-modif" data-id="' . $ingredient->id() . '"><img src="' . $ingredient->image() . '"><span>'
+                . $ingredient->name() . " </span><span class=qtx-unite>" . $ingredient->qtx() * $value . " " . $ingredient->unite()
+                . '</li>';
+        }
+        echo '</ul>';
+    }
+    ?>
+</article> 
