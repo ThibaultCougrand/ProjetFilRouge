b03d797a1a4a3cbf28e176912b63212232eeb958
Papytuc1 <kevinsanmartino@gmail.com>
2019-03-20 11:47:44 +0100

    affichage du prix de chaque ingredient et du total sur la page panier

--- vue/cart.php
+++ vue/cart.php
@@ -4,14 +4,22 @@
     foreach ($data['recette'] as $key => $value) {
         $list = $recette->ingredientsList($key);
         $uneRecette = $recette->recipe($key);
+        $priceRecipe = $recette->totalPrice($key);
+        if(count($list)>0){
         echo '<h2 class="titre-recette">' . $uneRecette->name() . '</h2>';
         echo '<ul class="ul-panier">';
+        
         foreach ($list as $ingredient) {
             echo '<li class="taille-filtre un-ingredient ingredient-modif" data-id="' . $ingredient->id() . '"><img src="' . $ingredient->image() . '"><span>'
                 . $ingredient->name() . " </span><span class=qtx-unite>" . $ingredient->qtx() * $value . " " . $ingredient->unite()
-                . '</li>';
+                .' '.$ingredient->price()*$value. '€</li>';
         }
-        echo '</ul>';
+        echo '</ul><div class="prix-total">'.$priceRecipe*$value.'</div>';
     }
+    else{
+        echo "<h2 class='titre-recette'>La recette : ". $uneRecette->name() . " n'a actuellement pas d'ingredient
+        veuillez nous excusez pour cette erreur</h2>";
+    }
+}
     ?>
 </article> 
