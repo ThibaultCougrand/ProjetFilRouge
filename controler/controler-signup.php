82409aea6aa1663cb40f01d2ae31784452dc6140
ThibaultCougrand <thibault.cougrand@gmail.com>
2019-03-21 10:15:58 +0100

    controller d'inscription verifie que tout les champ soient remplis et que verif email et password soient identique à email et password + requette ajax pour sibmit du formulaire

--- controler/controler-signup.php
+++ controler/controler-signup.php
@@ -1,20 +1,54 @@
 <?php
 $modele = new ModeleSignUp();
-$name = filter_input(INPUT_POST, 'nameIns');
-$firstName = filter_input(INPUT_POST, 'firstNameIns');
-$email = filter_input(INPUT_POST, 'emailIns');
-$password = filter_input(INPUT_POST, 'passwordIns');
-$age = filter_input(INPUT_POST, 'ageIns');
-$sex = filter_input(INPUT_POST, 'sexIns');
-
 $user = new ClassUser();
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+$verifEmail = filter_input(INPUT_POST, 'verifEmail');
+$verifPassword = filter_input(INPUT_POST, 'verifPassword');
+$name = filter_input(INPUT_POST, 'name');
+$firstName = filter_input(INPUT_POST, 'firstName');
+$age = filter_input(INPUT_POST, 'age');
+$sex = filter_input(INPUT_POST, 'sex');
+var_dump($email);
 
-$user->setName($name);
-$user->setFirstName($firstName);
-$user->setEmail($email);
-$user->setPassword($password);
-$user->setAge($age);
-$user->setSex($sex);
-//if ($modele->verifEmailExist($user)) {
-$modele->inscription($user);
-//}
+$data = [];
+
+if ($email === $verifEmail) {
+    $user->setEmail($email);
+    $data['email'] = "";
+} else {
+    $data['email'] = "L'email de verification ne corespond pas avec l'email saisi.";
+}
+if ($password === $verifPassword) {
+    $user->setPassword($password);
+    $data['password'] = "";
+} else {
+    $data['password'] = "Le mot de passe de verification ne corespond pas avec le mot de passe saisi.";
+}
+if (!empty($name)) {
+    $user->setName($name);
+    $data['name'] = "";
+} else {
+    $data['name'] = "Veuillez saisir un nom";
+}
+if (!empty($firstName)) {
+    $user->setFirstName($firstName);
+    $data['firstName'] = "";
+} else {
+    $data['firstName'] = "Veuillez saisir un prenom";
+}
+if (!empty($age)) {
+    $user->setAge($age);
+    $data['age'] = "";
+} else {
+    $data['age'] = "Veuillez saisir un age";
+}
+if (!empty($sex)) {
+    $user->setSex($sex);
+    $data['sex'] = "";
+} else {
+    $data['sex'] = "Veuillez choisir un sexe";
+}
+
+echo json_encode($data);
+//$modele->inscription($user);
--- js/main.js
+++ js/main.js
@@ -6,32 +6,32 @@ $(".category-recipe").click(function () {
     reset = $("#les-recettes");
     reset.html("");
     $.post(
-        'ajax.php', {
-            id: $(this).data("id"),
-            uc: 'recipe'
-        },
-        function (data) {
-            results = (JSON.parse(data));
-            if (Array.isArray(results)) {
-                results.forEach(element => {
-                    console.log(element);
-                    figure = document.createElement('figure');
-                    lien = document.createElement('a');
-                    lien.href = "index.php?loc=one-recipe&id=" + element.id;
-                    img = document.createElement('div');
-                    figcaption = document.createElement('figcaption');
-                    img.className = "image-recette";
-                    img.style = "background-image: url(" + element.img + ")";
-                    figcaption.textContent = element.name;
-                    lien.append(img);
-                    figure.append(lien);
-                    figure.append(figcaption);
-                    $("#les-recettes").append(figure);
-                });
-            } else {
-                console.log('pas tableau');
+            'ajax.php', {
+                id: $(this).data("id"),
+                uc: 'recipe'
+            },
+            function (data) {
+                results = (JSON.parse(data));
+                if (Array.isArray(results)) {
+                    results.forEach(element => {
+                        console.log(element);
+                        figure = document.createElement('figure');
+                        lien = document.createElement('a');
+                        lien.href = "index.php?loc=one-recipe&id=" + element.id;
+                        img = document.createElement('div');
+                        figcaption = document.createElement('figcaption');
+                        img.className = "image-recette";
+                        img.style = "background-image: url(" + element.img + ")";
+                        figcaption.textContent = element.name;
+                        lien.append(img);
+                        figure.append(lien);
+                        figure.append(figcaption);
+                        $("#les-recettes").append(figure);
+                    });
+                } else {
+                    console.log('pas tableau');
+                }
             }
-        }
     );
 });
 
@@ -42,12 +42,12 @@ $(".category-recipe").click(function () {
 $(".ajout-panier").click(function () {
     let params = new URLSearchParams(document.location.search);
     let id = params.get("id");
-    var tableauId = {"recette":{}};
+    var tableauId = {"recette": {}};
     monCookie = Cookies.get('panier');
     if (monCookie !== undefined) {
         tableauId = JSON.parse(monCookie);
     }
-    console.log("tab en json "+tableauId["recette"][id]);
+    console.log("tab en json " + tableauId["recette"][id]);
     if (tableauId["recette"][id] === undefined) {
         tableauId["recette"][id] = 1;
     } else {
@@ -62,31 +62,61 @@ $(".ajout-panier").click(function () {
 /*************************************/
 
 function afficheFormulaire() {
-    console.log(document.querySelector("#emailIns").value);
     $.post(
             'ajax.php', {
-                email: document.querySelector("#emailIns").value,
+                email: $("#emailIns").val(),
                 uc: 'signup'
             },
             function (data) {
                 console.log(data);
                 var results = JSON.parse(data);
-                if (results.verif === true && document.querySelector('#emailIns').value !== "") {
-                    $("#buttonIns").hide();
-                    $("#labelEmailIns").hide();
-                    $("#emailIns").hide();
-                    $("#labelPasswordIns").hide();
-                    $("#passwordIns").hide();
-                    afficheSuiteForm();
+                var email = $('#emailIns').val().toString();
+                console.log(!email.substring(email.indexOf("@"), email.length).includes("."));
+                if (email === "") {
+                    document.querySelector('#err-email').textContent = "Veuillez saisir un email";
+                } else if (!email.includes("@") || !email.substring(email.indexOf("@"), email.length).includes(".")) {
+                    document.querySelector('#err-email').textContent = "email invalide !";
+                } else if (results.verif === true) {
+                    document.querySelector('#err-email').textContent = "";
+                    if ($('#passwordIns').val() !== "") {
+                        document.querySelector('#err-password').textContent = "";
+                        $("#buttonIns").hide();
+                        $("#labelEmailIns").hide();
+                        $("#emailIns").hide();
+                        $("#labelPasswordIns").hide();
+                        $("#passwordIns").hide();
+                        afficheSuiteForm();
+                    } else {
+                        document.querySelector('#err-password').textContent = "Veuillez saisir un mot de passe";
+                    }
                 } else {
                     document.querySelector('#err-email').textContent = "Email invalide !";
                 }
             });
 }
 
+/*Requette ajax pour valider l'inscription*/
+$('#submitIns').click(function (e) {
+    console.log($('#emailIns').val());
+    $POST(
+            'controler/controler-signup.php', {
+                email: $('#emailIns').val(),
+                password: $('#passwordIns').val(),
+                verifEmail: $('#verifEmailIns').val(),
+                verifPassword: $('#verifPasswordIns').val(),
+                name: $('#nameIns').val(),
+                firstName: $('#firstNameIns').val(),
+                age: $('#ageIns').val(),
+                sex: $("input[@name=sexIns][@checked]").val()
+            },
+            function (data) {
+
+            });
+});
+
 /*Fonction qui génère le formulaire d'inscription dans la page html*/
 function afficheSuiteForm() {
-    
+
     /*br,hr et div radio*/
     var separate = document.createElement('hr');
     separate.className = "dividerIns";
@@ -95,7 +125,7 @@ function afficheSuiteForm() {
     var lineBreack3 = document.createElement('br');
     var divRadio = document.createElement('div');
     divRadio.className = "divRadio";
-    
+
     /*création de mes éléments*/
     // verif email.
     var labelVerifEmail = document.createElement('label');
@@ -103,6 +133,7 @@ function afficheSuiteForm() {
     var verifEmail = document.createElement('input');
     verifEmail.type = 'text';
     verifEmail.name = 'verifEmailIns';
+    verifEmail.id = 'verifEmailIns';
     var errVerifEmail = document.createElement('p');
     errVerifEmail.id = "err-verif-email";
     errVerifEmail.className = "mesErr";
@@ -111,7 +142,8 @@ function afficheSuiteForm() {
     labelVerifPassword.textContent = "vérifiez votre mot de passe";
     var verifPassword = document.createElement('input');
     verifPassword.type = 'text';
-    verifPassword.name = 'verifPAsswordIns';
+    verifPassword.name = 'verifPasswordIns';
+    verifPassword.id = 'verifPasswordIns';
     var errVerifPassword = document.createElement('p');
     errVerifPassword.id = "err-verif-password";
     errVerifPassword.className = "mesErr";
@@ -121,6 +153,7 @@ function afficheSuiteForm() {
     var name = document.createElement('input');
     name.type = "text";
     name.name = "nameIns";
+    name.id = 'nameIns';
     var errName = document.createElement('p');
     errName.id = "err-name";
     errName.className = "mesErr";
@@ -130,6 +163,7 @@ function afficheSuiteForm() {
     var firstName = document.createElement('input');
     firstName.type = "text";
     firstName.name = "firstNameIns";
+    firstName.id = 'firstNameIns';
     var errFirstName = document.createElement('p');
     errFirstName.id = "err-firstname";
     errFirstName.className = "mesErr";
@@ -141,6 +175,7 @@ function afficheSuiteForm() {
     age.min = "1";
     age.max = "99";
     age.name = "ageIns";
+    age.id = 'ageIns';
     var errAge = document.createElement('p');
     errAge.id = "err-age";
     errAge.className = "mesErr";
@@ -171,9 +206,10 @@ function afficheSuiteForm() {
     // bouton
     var button = document.createElement('input');
     button.type = "submit";
+    button.id = "submitIns";
     button.value = "S'inscrire";
     button.form = "inscription";
-    
+
     /*push de mes éléments dans le html.*/
     $('#inscription').append(labelVerifEmail);
     $('#inscription').append(verifEmail);
@@ -215,46 +251,46 @@ $('.category-ing').click(function () {
     reset = $('#les-articles');
     reset.html(""); //me permet de reset la class qui contient les images, names etc
     $.post(
-        'ajax.php', { //permet de pointe vers la page ajax.php
-            id: $(this).data("id"), //envoie la valeur de $id dans ajax.php
-            uc: 'produit' //permet de switch dans le ajax.php 
-        },
-        function (data) {
-            //retour de data dans ajax.php
-            results = (JSON.parse(data)); //JE PARSE MON TABLEAU EN JSON
-            if (Array.isArray(results)) {
-                results.forEach(element => {
-                    console.log(data);
-                    figure = document.createElement('figure');
-                    lien = document.createElement('a');
-                    lien.href = "index.php?loc=produits&id=" + element.id;
-                    img = document.createElement('img');
-                    figcaption = document.createElement('figcaption');
-                    div = document.createElement('div');
-                    div.className = "wrap-button";
-                    para = document.createElement('p');
-                    para.textContent = element.prix + '€' + ' pour '+ element.qtx + ' ' + element.unit;
-                    button = document.createElement('button');
-                    i = document.createElement('i');
-                    i.className = "fas fa-cart-plus";
-                    img.src=element.img;
-                    figcaption.textContent = element.name;
+            'ajax.php', {//permet de pointe vers la page ajax.php
+                id: $(this).data("id"), //envoie la valeur de $id dans ajax.php
+                uc: 'produit' //permet de switch dans le ajax.php 
+            },
+            function (data) {
+                //retour de data dans ajax.php
+                results = (JSON.parse(data)); //JE PARSE MON TABLEAU EN JSON
+                if (Array.isArray(results)) {
+                    results.forEach(element => {
+                        console.log(data);
+                        figure = document.createElement('figure');
+                        lien = document.createElement('a');
+                        lien.href = "index.php?loc=produits&id=" + element.id;
+                        img = document.createElement('img');
+                        figcaption = document.createElement('figcaption');
+                        div = document.createElement('div');
+                        div.className = "wrap-button";
+                        para = document.createElement('p');
+                        para.textContent = element.prix + '€' + ' pour ' + element.qtx + ' ' + element.unit;
+                        button = document.createElement('button');
+                        i = document.createElement('i');
+                        i.className = "fas fa-cart-plus";
+                        img.src = element.img;
+                        figcaption.textContent = element.name;
 
-                    button.append(i);
-                    div.append(para); 
-                    div.append(button);
-                    lien.append(img);
-                    figure.append(lien);
-                    figure.append(figcaption);
-                    figure.append(div);
-                    $('#les-articles').append(figure);
+                        button.append(i);
+                        div.append(para);
+                        div.append(button);
+                        lien.append(img);
+                        figure.append(lien);
+                        figure.append(figcaption);
+                        figure.append(div);
+                        $('#les-articles').append(figure);
 
-                });
-            } else {
-                console.log('requête ajax romain null');
-            }
-        });
-        $(".image-recette").click(function () {
+                    });
+                } else {
+                    console.log('requête ajax romain null');
+                }
+            });
+    $(".image-recette").click(function () {
 
-        });
+    });
 });
